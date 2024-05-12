6931f8fbfe00b6a747206939ca19b57fe7267047
Olga dé claire <olgafadillah@proton.me>
2024-05-12 22:03:20 +0700

    Update kranul-build.sh

--- kranul-build.sh
+++ kranul-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2022-2023 Neebe3289 <neebexd@gmail.com>
+# Copyright (C) 2022-2023, Neebe3289 <neebexd@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,225 +15,307 @@
 # limitations under the License.
 #
 # Personal script for kranul compilation !!
+# Inspired the script from Panchajanya1999 based.
+#
 
-# Load variables from config.env
-export $(grep -v '^#' config.env | xargs)
-
-# Path
-MainPath="$(readlink -f -- $(pwd))"
-MainClangPath="${MainPath}/clang"
-AnyKernelPath="${MainPath}/anykernel"
-CrossCompileFlagTriple="aarch64-linux-gnu-"
-CrossCompileFlag64="aarch64-linux-gnu-"
-CrossCompileFlag32="arm-linux-gnueabi-"
-
-# Clone toolchain
-[[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
-function getclang() {
-  if [ "${ClangName}" = "azure" ]; then
-    if [ ! -f "${MainClangPath}-azure/bin/clang" ]; then
-      echo "[!] Clang is set to azure, cloning it..."
-      git clone https://gitlab.com/Panchajanya1999/azure-clang clang-azure --depth=1
-      ClangPath="${MainClangPath}"-azure
-      export PATH="${ClangPath}/bin:${PATH}"
-      cd ${ClangPath}
-      wget "https://gist.github.com/dakkshesh07/240736992abf0ea6f0ee1d8acb57a400/raw/a835c3cf8d99925ca33cec3b210ee962904c9478/patch-for-old-glibc.sh" -O patch.sh && chmod +x patch.sh && ./patch.sh
-      cd ..
-    else
-      echo "[!] Clang already exists. Skipping..."
-      ClangPath="${MainClangPath}"-azure
-      export PATH="${ClangPath}/bin:${PATH}"
-    fi
-  elif [ "${ClangName}" = "neutron" ] || [ "${ClangName}" = "" ]; then
-    if [ ! -f "${MainClangPath}-neutron/bin/clang" ]; then
-      echo "[!] Clang is set to neutron, cloning it..."
-      mkdir -p "${MainClangPath}"-neutron
-      ClangPath="${MainClangPath}"-neutron
-      export PATH="${ClangPath}/bin:${PATH}"
-      cd ${ClangPath}
-      curl -LOk "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
-      chmod +x antman && ./antman -S
-      ./antman --patch=glibc
-      cd ..
-    else
-      echo "[!] Clang already exists. Skipping..."
-      ClangPath="${MainClangPath}"-neutron
-      export PATH="${ClangPath}/bin:${PATH}"
-    fi
-  elif [ "${ClangName}" = "proton" ]; then
-    if [ ! -f "${MainClangPath}-proton/bin/clang" ]; then
-      echo "[!] Clang is set to proton, cloning it..."
-      git clone https://github.com/kdrag0n/proton-clang.git -b master clang-proton --depth=1
-      ClangPath="${MainClangPath}"-proton
-      export PATH="${ClangPath}/bin:${PATH}"
-    else
-      echo "[!] Clang already exists. Skipping..."
-      ClangPath="${MainClangPath}"-proton
-      export PATH="${ClangPath}/bin:${PATH}"
-    fi
-  elif [ "${ClangName}" = "zyc" ]; then
-    if [ ! -f "${MainClangPath}-zyc/bin/clang" ]; then
-      echo "[!] Clang is set to zyc, cloning it..."
-      mkdir -p ${MainClangPath}-zyc
-      cd clang-zyc
-      wget -q $(curl -k https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-link.txt 2>/dev/null) -O "zyc-clang.tar.gz"
-      tar -xf zyc-clang.tar.gz
-      ClangPath="${MainClangPath}"-zyc
-      export PATH="${ClangPath}/bin:${PATH}"
-      rm -f zyc-clang.tar.gz
-      cd ..
-    else
-      echo "[!] Clang already exists. Skipping..."
-      ClangPath="${MainClangPath}"-zyc
-      export PATH="${ClangPath}/bin:${PATH}"
-    fi
-  else
-    echo "[!] Incorrect clang name. Check config.env for clang names."
-    exit 1
-  fi
-  if [ ! -f '${MainClangPath}-${ClangName}/bin/clang' ]; then
-    export KBUILD_COMPILER_STRING="$(${MainClangPath}-${ClangName}/bin/clang --version | head -n 1)"
-  else
-    export KBUILD_COMPILER_STRING="Unknown"
-  fi
+# Function to show an informational message.
+msg()
+{
+    echo -e "\e[1;32m$*\e[0m"
 }
 
-function updateclang() {
-  [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
-  if [ "${ClangName}" = "neutron" ] || [ "${ClangName}" = "" ]; then
-    echo "[!] Clang is set to neutron, checking for updates..."
-    cd clang-neutron
-    if [ "$(./antman -U | grep "Nothing to do")" = "" ];then
-      ./antman --patch=glibc
-    else
-      echo "[!] No updates have been found, skipping"
-    fi
-    cd ..
-    elif [ "${ClangName}" = "zyc" ]; then
-      echo "[!] Clang is set to zyc, checking for updates..."
-      cd clang-zyc
-      ZycLatest="$(curl -k https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt)"
-      if [ "$(cat README.md | grep "Build Date : " | cut -d: -f2 | sed "s/ //g")" != "${ZycLatest}" ];then
-        echo "[!] An update have been found, updating..."
-        sudo rm -rf ./*
-        wget -q $(curl -k https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-link.txt 2>/dev/null) -O "zyc-clang.tar.gz"
-        tar -xf zyc-clang.tar.gz
-        rm -f zyc-clang.tar.gz
-      else
-        echo "[!] No updates have been found, skipping..."
-      fi
-      cd ..
-    elif [ "${ClangName}" = "azure" ]; then
-      cd clang-azure
-      git fetch -q origin main
-      git pull origin main
-      cd ..
-    elif [ "${ClangName}" = "proton" ]; then
-      cd clang-proton
-      git fetch -q origin master
-      git pull origin master
-      cd ..
-  fi
+err()
+{
+    echo -e "\e[1;31m$*\e[0m"
 }
 
-# Enviromental variable
-DEVICE_MODEL="Mi439"
-DEVICE_CODENAME=""
-export DEVICE_DEFCONFIG="lineageos_mi439_defconfig"
-export ARCH="arm64"
-export KBUILD_BUILD_USER="Dréams"
-export KBUILD_BUILD_HOST="Soulvibe"
-export KERNEL_NAME="$(cat "arch/arm64/configs/$DEVICE_DEFCONFIG" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )"
-export SUBLEVEL="v4.19.$(cat "${MainPath}/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')"
-IMAGE="${MainPath}/out/arch/arm64/boot/Image.gz-dtb"
-CORES="$(nproc --all)"
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-
-# Start Compile
-START=$(date +"%s")
-
-compile(){
-if [ "$ClangName" = "proton" ]; then
-  sed -i 's/CONFIG_LLVM_POLLY=y/# CONFIG_LLVM_POLLY is not set/g' ${MainPath}/arch/$ARCH/configs/$DEVICE_DEFCONFIG || echo ""
-else
-  sed -i 's/# CONFIG_LLVM_POLLY is not set/CONFIG_LLVM_POLLY=y/g' ${MainPath}/arch/$ARCH/configs/$DEVICE_DEFCONFIG || echo ""
+# Check telegtam token/id
+if [ -z "6410284454:AAESx0jgdzy-z4W0t-Oo53NaaS-mhGka8_4" ] || [ -z "-1002088104319" ]
+then
+    err "Missing environment! .Please check again . ."
+    exit
 fi
-make O=out ARCH=$ARCH $DEVICE_DEFCONFIG
-make -j"$CORES" ARCH=$ARCH O=out \
-    CC=clang \
-    LD=ld.lld \
-    LLVM=1 \
-    LLVM_IAS=1 \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip \
-    CLANG_TRIPLE=${CrossCompileFlagTriple} \
-    CROSS_COMPILE=${CrossCompileFlag64} \
-    CROSS_COMPILE_ARM32=${CrossCompileFlag32}
 
-   if [[ -f "$IMAGE" ]]; then
-      cd ${MainPath}
-      cp out/.config arch/${ARCH}/configs/${DEVICE_DEFCONFIG} && git add arch/${ARCH}/configs/${DEVICE_DEFCONFIG} && git commit -m "defconfig: Regenerate"
-      git clone --depth=1 ${AnyKernelRepo} -b ${AnyKernelBranch} ${AnyKernelPath}
-      cp $IMAGE ${AnyKernelPath}
-   else
-      echo "❌ Compile Kernel for $DEVICE_CODENAME failed, Check console log to fix it!"
-      if [ "$CLEANUP" = "yes" ];then
-        cleanup
-      fi
-      exit 1
-   fi
+#####################
+# Basic Information #
+#####################
+
+# Set main directory of kernel source.
+MAIN_DIR="$(pwd)"
+
+# Specify name of device model.
+# e.g: 'Redmi note 8 Pro'
+DEVICE_MODEL="Mi Sdm439"
+
+# Specify name of device codename.
+# e.g: 'begonia'
+DEVICE_CODENAME="mi439"
+
+# Set default achitecture.
+ARCH=arm64
+
+# Set device defconfig name.
+# e.g: 'begonia_user_defconfig'
+DEVICE_DEFCONFIG=lineageos_mi439_defconfig
+
+# Set default image files/artifacts.
+IMAGE=Image.gz-dtb
+
+# Specify kernel name for ZIP name
+KERNEL_NAME="TesT"
+
+# Check kernel version.
+SUBLEVEL=$(make kernelversion)
+
+# Grab git current branch.
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Grab git commit hash.
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+# Set date into ZIP name.
+DATE=$(date +"%Y%m%d-%H%M")
+
+# Specify command to get KernelSU function.
+# 'n' is NO(default) | 'y' is YES
+KERNELSU=n
+
+# Specify command to update submodule for KernelSU.
+# 'n' is NO(default) | 'y' is YES
+SUBMODULE=n
+
+# Specify command name to set default compiler or toolchain.
+# 'aosp'(default) | 'azure' | 'neutron' | 'proton'| 'eva'
+TOOLCHAIN=eva
+
+# Clone compiler, anykernel3 and telegram.sh
+clone()
+{
+     if [ "$TOOLCHAIN" = "aosp" ]
+     then
+          msg "Clone latest aosp clang toolchain"
+          wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229.tar.gz -O "aosp-clang.tar.gz"
+          mkdir clang-llvm && tar -xf aosp-clang.tar.gz -C clang-llvm && rm -rf aosp-clang.tar.gz
+          git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-19.1 gcc64
+          git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 -b lineage-19.1 gcc32
+     elif [ "$TOOLCHAIN" = "azure" ]
+     then
+          msg "Clone latest azure clang toolchain"
+          git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang-llvm
+     elif [ "$TOOLCHAIN" = "neutron" ]
+     then
+          msg "Clone latest neutron clang toolchain"
+          mkdir clang-llvm
+          cd clang-llvm
+          curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
+          chmod +x antman && bash antman -S=latest
+          bash antman --patch=glibc
+          cd ..
+     elif [ "$TOOLCHAIN" = "proton" ]
+     then
+          msg "Clone latest proton clang toolchain"
+          git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang-llvm
+     elif [ "$TOOLCHAIN" = "eva" ]
+     then
+          msg "Clone latest eva gcc toolchain"
+          git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc-arm64
+          git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc-arm
+     fi
+
+     msg "Clone AnyKernel3 source"
+     git clone --depth=1 https://github.com/Neebe3289/AnyKernel3 -b begonia AnyKernel3
+
+     msg "Clone telegram.sh source"
+     git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
 }
 
-# Zipping function
-function zipping() {
-  if [[ -z "${AnyKernelPath}" ]]; then
-    echo "Error: AnyKernelPath is not defined."
-    return 1
-  fi
+# Export
+exports()
+{
+       # Set Indonesian timezone
+       TZ="Asia/Jakarta"
 
-  cd "${AnyKernelPath}" || { echo "Error: Failed to change directory to ${AnyKernelPath}"; return 1; }
+       # Specify user name and host name
+       KBUILD_BUILD_USER="Dreams"
+       KBUILD_BUILD_HOST="Soulvibe"
 
-  sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})/g" anykernel.sh
+       if [ "$TOOLCHAIN" = "aosp" ]
+       then
+            PATH=$MAIN_DIR/clang-llvm/bin:$MAIN_DIR/gcc64/bin:$MAIN_DIR/gcc32/bin:$PATH
+            LD_LIBRARY_PATH=$MAIN_DIR/clang-llvm/lib:$LD_LIBRARY_PATH
+            KBUILD_COMPILER_STRING=$("$MAIN_DIR"/clang-llvm/bin/clang --version | head -n 1)
+            COMPILER=$KBUILD_COMPILER_STRING
+       elif [ "$TOOLCHAIN" = "azure" ] || [ "$TOOLCHAIN" = "neutron" ] || [ "$TOOLCHAIN" = "proton" ]
+       then
+            PATH=$MAIN_DIR/clang-llvm/bin:$PATH
+            KBUILD_COMPILER_STRING=$("$MAIN_DIR"/clang-llvm/bin/clang --version | head -n 1)
+            COMPILER=$KBUILD_COMPILER_STRING
+       elif [ "$TOOLCHAIN" = "eva" ]
+       then
+            PATH=$MAIN_DIR/gcc-arm64/bin:$MAIN_DIR/gcc-arm/bin:$PATH
+            KBUILD_COMPILER_STRING=$("$MAIN_DIR"/gcc-arm64/bin/aarch64-elf-gcc --version | head -n 1)
+            COMPILER=$KBUILD_COMPILER_STRING
+       fi
 
-  zip -r9 "[${KERNEL_VARIANT}]"-${KERNEL_NAME}-${SUBLEVEL}-${DEVICE_CODENAME}.zip * -x .git README.md *placeholder
+       # Specify CPU core/thread for compilation.
+       # e.g: '2'/'4'/'8'/'12' or set default by using 'nproc --all'
+       CORES=$(nproc --all)
 
-  cd ..
-  mkdir -p builds
+       # Telegram directory.
+       TELEGRAM=$MAIN_DIR/telegram/telegram
 
-  zipname="$(basename $(ls -t ${AnyKernelPath}/*.zip | head -n 1))"  # More reliable way to get the latest zip
-  cp ${AnyKernelPath}/${zipname} ./builds/${zipname%.*}-$DATE.zip  # Remove .zip extension before adding date
-
-  cleanup
+       export TZ ARCH DEVICE_DEFCONFIG KBUILD_BUILD_USER KBUILD_BUILD_HOST \
+              PATH KBUILD_COMPILER_STRING COMPILER CORES \
+              TELEGRAM
 }
 
-# Cleanup function
-function cleanup() {
-    cd ${MainPath}
-    sudo rm -rf ${AnyKernelPath}
-    sudo rm -rf out/
+# Function to show an informational message to telegram.
+send_msg()
+{
+    "${TELEGRAM}" -H -D \
+        "$(
+            for POST in "${@}"; do
+                echo "${POST}"
+            done
+        )"
 }
 
-# KernelSU function
-function kernelsu() {
-    if [ "$KERNELSU" = "yes" ];then
-      KERNEL_VARIANT="${KERNEL_VARIANT}-KernelSU"
-      if [ ! -f "${MainPath}/KernelSU/README.md" ]; then
-        cd ${MainPath}
-        curl -LSsk "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-        sed -i "s/CONFIG_KSU=n/CONFIG_KSU=y/g" arch/${ARCH}/configs/${DEVICE_DEFCONFIG}
-      fi
-      KERNELSU_VERSION="$((10000 + $(cd KernelSU && git rev-list --count HEAD) + 200))"
-      git submodule update --init; cd ${MainPath}/KernelSU; git pull origin main; cd ..
+send_file()
+{
+    "${TELEGRAM}" -H \
+        -f "$1" \
+        "$2"
+}
+
+# Function for KernelSU.
+# This is the default setting from my own.
+# If facing loop issue when you added KernelSU support manually to your own kernel source, don't try to enable 'CONFIG_KPROBES' in your defconfig.
+# It is known that 'KPROBES' is broken in some cases, which is causing loop. so never activate it.
+# Reference: https://github.com/tiann/KernelSU/pull/453.
+kernelsu()
+{
+    if [ "$SUBMODULE" = "y" ]
+    then
+       if [ ! -d "$MAIN_DIR/KernelSU" ]
+       then
+         msg "Do update submodule for kernelsu"
+         cd "$MAIN_DIR"
+         git submodule update --init --recursive
+         git submodule update --remote --recursive
     fi
+
+    if [ "$KERNELSU" = "y" ]
+    then
+       if [ ! -d "$MAIN_DIR/KernelSU" ]
+       then
+         msg "Do make kernelsu functional"
+         cd "$MAIN_DIR"
+         curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+         echo "CONFIG_KSU=y" >> arch/arm64/configs/$DEVICE_DEFCONFIG
+         echo "CONFIG_KSU_DEBUG=y" >> arch/arm64/configs/$DEVICE_DEFCONFIG
+         echo "CONFIG_OVERLAY_FS=y" >> arch/arm64/configs/$DEVICE_DEFCONFIG
+    fi
+    rm -rf KernelSU
+    git clone https://github.com/tiann/KernelSU -b main
 }
 
-getclang
-updateclang
-kernelsu
+# Make it ZIP.
+make_zip()
+{
+    msg "Make it a flashable ZIP files.."
+    ZIPNAME="$KERNEL_NAME-$DEVICE_CODENAME-$COMMIT_HASH-$DATE"
+    ZIP_FINAL="$ZIPNAME.zip"
+    cd AnyKernel3 || exit 1
+    sed -i "s/kernel.string=.*/kernel.string=$KERNEL_NAME by $KBUILD_BUILD_USER/g" anykernel.sh
+    zip -r9 "$ZIP_FINAL" ./* -x .git .gitignore README.md *placeholder ./*.zip
+    cd ..
+}
+
+# Upload ZIP files to Telegram.
+send_zip()
+{
+    msg "Start to upload ZIP files.."
+    cd AnyKernel3
+    ZIPFILE=$(echo *.zip)
+    SHA1=$(sha1sum "$ZIPFILE" | cut -d' ' -f1)
+    send_file "$ZIPFILE" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) for $DEVICE_CODENAME | SHA1 : <code>$SHA1</code>"
+}
+
+# Function for upload error log during compiled.
+send_log()
+{
+    ERROR_LOG=$(echo error.log)
+    send_file "$ERROR_LOG" "❌ Build failed to compile, Please check log to fix it!"
+    exit 1
+}
+
+# Compilation setup.
+compile()
+{
+     BUILD_START=$(date +"%s")
+     kernelsu
+     send_msg "<b>============================================</b>" \
+        "<b>• DATE :</b> <code>$(TZ=Asia/Jakarta date +"%A, %d %b %Y, %H:%M:%S")</code>" \
+        "<b>• DEVICE :</b> <code>$DEVICE_MODEL [$DEVICE_CODENAME]</code>" \
+        "<b>• KERNEL NAME :</b> <code>$KERNEL_NAME</code>" \
+        "<b>• LINUX VERSION :</b> <code>$SUBLEVEL</code>" \
+        "<b>• BRANCH NAME :</b> <code>$BRANCH</code>" \
+        "<b>• COMPILER :</b> <code>$COMPILER</code>" \
+        "<b>• LAST COMMIT :</b> <code>$(git log --pretty=format:'%s' -1)</code>" \
+        "<b>============================================</b>" \
+
+     if [ "$TOOLCHAIN" = "aosp" ]
+     then
+          MAKE+=(
+              LLVM=1 \
+              LLVM_IAS=1 \
+              CLANG_TRIPLE=aarch64-linux-gnu- \
+              CROSS_COMPILE=aarch64-linux-android- \
+              CROSS_COMPILE_ARM32=arm-linux-androideabi-
+          )
+     elif [ "$TOOLCHAIN" = "azure" ] || [ "$TOOLCHAIN" = "neutron" ] || [ "$TOOLCHAIN" = "proton" ]
+     then
+          MAKE+=(
+              LLVM=1 \
+              LLVM_IAS=1 \
+              CROSS_COMPILE=aarch64-linux-gnu- \
+              CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+          )
+     elif [ "$TOOLCHAIN" = "eva" ]
+     then
+          MAKE+=(
+              AR=aarch64-elf-ar \
+              LD=aarch64-elf-ld.lld \
+              NM=aarch64-elf-nm \
+              OBCOPY=aarch64-elf-objcopy \
+              OBJDUMP=aarch64-elf-objdump \
+              STRIP=aarch64-elf-strip \
+              CROSS_COMPILE=aarch64-elf- \
+              CROSS_COMPILE_ARM32=arm-eabi-
+          )
+     fi
+
+     msg "Compilation has been started.."
+     make O=out ARCH=arm64 $DEVICE_DEFCONFIG
+     make -j"$CORES" ARCH=arm64 O=out \
+           "${MAKE[@]}" 2>&1 | tee error.log
+
+     if ! [ -a "$MAIN_DIR"/out/arch/arm64/boot/$IMAGE ]
+     then
+          err "Build failed to compile, check log to fix it!"
+          send_log
+          exit 1
+     else
+          msg "Kernel succesfully to compile!"
+          cp "$MAIN_DIR"/out/arch/arm64/boot/$IMAGE AnyKernel3
+     fi
+}
+
+clone
+exports
 compile
-zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
+BUILD_END=$(date +"%s")
+DIFF=$((BUILD_END - BUILD_START))
+make_zip
+send_zip
+
